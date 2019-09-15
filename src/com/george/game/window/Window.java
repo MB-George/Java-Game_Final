71e57777094f5995bf05c8b7d05e027175a8ec0c
MB-George <54776079+MB-George@users.noreply.github.com>
2019-09-15 10:50:29 -0500

    Added Icon
    Change the frame to the icon

--- src/com/george/game/window/Window.java
+++ src/com/george/game/window/Window.java
@@ -1,7 +1,10 @@
 package com.george.game.window;
 
 import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
 import com.george.game.Game;
 
 public class Window
@@ -11,10 +14,15 @@ public class Window
     String title;
     
     public Window(final String title, final int height, final int width, final Game game) {
+    	ImageIcon icon = new ImageIcon("C:\\Users\\ggl20\\Documents\\GitHub\\Java-Game_Final\\res\\images\\icon.jpg");
+    	
         JFrame frame = new JFrame(title);
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
+        
+        //Icon Code
+        frame.setIconImage(icon.getImage());
         
         frame.add(game);
         frame.setResizable(true);
